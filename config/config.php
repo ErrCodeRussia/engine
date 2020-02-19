6b52628d513f358de90e60653e8fcc8d62695eb7
Pavel Sklyar <ozpavel@yandex.ru>
2020-02-19 16:04:27 +0300

    Обновление конфигурации

--- config/config.php
+++ config/config.php
@@ -1,27 +1,29 @@
 <?php
 
+$database = require __DIR__ . "/database.php";
+
 return [
+    /** Название проекта */
     'name' => 'ErrCode Base Project',
+
+    /** Относительные ссылки на главную страницу и страницу авторизации */
     'homeUrl' => '/',
+    'authUrl' => '/auth/',
 
-    'database' => [
-        'default' => [
-            'host' => 'localhost',
-            'user' => 'homestead',
-            'password' => 'secret',
-            'database' => ''
-        ],
-    ],
+    'database' => $database,
 
+    /** Названия файлов стилей из public_html/css/, которые нужно подключить */
     'styles' => [
         'main.css',
         'fonts.css'
     ],
 
+    /** Названия скриптовых файлов из public_html/js/, которые нужно подключить */
     'scripts' => [
 
     ],
 
+    /** Ссылка на favicon относительно public_html/ */
     'favicon' => '',
 
     'errors' => [
